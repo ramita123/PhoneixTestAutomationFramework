12417a6ccd0eafafa008341587cdae7f5f8ae294
ramita123 <ramitasambyal@gmail.com>
2026-05-04 22:56:52 +0530

    configManager for removing hardcoded values and Environment switches

--- src/test/java/com/api/test/LoginApiTest.java
+++ src/test/java/com/api/test/LoginApiTest.java
@@ -4,10 +4,15 @@ import static io.restassured.RestAssured.*;
 
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import com.api.pojo.UserCredentials;
+
+
+import static com.api.utils.ConfigManager2.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -15,12 +20,14 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 public class LoginApiTest {
 	
 	@Test
-	public void loginApiTest() {
+	public void loginApiTest() throws IOException {
 
 		
 		UserCredentials userCredentials=new UserCredentials("iamfd","password");
+		//ConfigManager configManager= null;
 		
-		given().baseUri("http://64.227.160.186:9000/v1").and().contentType(ContentType.JSON).and().accept(ContentType.JSON).
+		
+		given().baseUri(getProperty("BASE_URI")).and().contentType(ContentType.JSON).and().accept(ContentType.JSON).
 		and().body(userCredentials).
 		log().uri().and().log().headers().and().log().method().and().
 		log().body().when().post("/login").then().log().all().statusCode(200).and().time(lessThan(1000l))
---
+++ src/test/java/com/api/utils/ConfigManager2.java
@@ -0,0 +1,73 @@
+package com.api.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager2 {
+
+	// wap to read the peroperty file from
+	// src/java/resources/config/config.properties
+	private static Properties prop = new Properties();
+	private static String filePath;
+	private static String env;
+
+	private ConfigManager2() {
+
+	}
+
+	// static block going to execute only once during the class loading
+	static {
+
+		env = System.getProperty("env","qa");
+		env= env.toLowerCase().trim();
+
+		switch (env) {
+
+		case "dev": {
+
+			filePath = "config" + File.separator + "config.dev.properties";
+			break;
+		}
+		case "qa": {
+
+			filePath = "config" + File.separator + "config.qa.properties";
+			break;
+		}
+
+		case "uat": {
+
+			filePath = "config"+File.separator+"config.uat.properties";
+			break;
+		}
+
+		default: {
+
+			filePath = "config" + File.separator + "config.properties";
+			
+		}
+
+		}
+	
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+
+		if (inputStream == null) {
+			throw new RuntimeException("cannot find the file at path" + filePath);
+		}
+		try {
+			prop.load(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static String getProperty(String key) throws IOException {
+
+		return prop.getProperty(key);
+
+	}
+}
