cf46399ca010ead094f719157367722e24b91ea1
ramita123 <ramitasambyal@gmail.com>
2026-05-06 12:58:42 +0530

    created auth token provider to manage the token for different roles

---
+++ src/test/java/com/api/test/CountAPITest.java
@@ -0,0 +1,45 @@
+package com.api.test;
+
+import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import com.api.constant.Role;
+import static com.api.utils.AuthTokenProvider.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+import static com.api.utils.ConfigManager2.*;
+
+import static io.restassured.RestAssured.*;
+
+public class CountAPITest {
+
+	@Test
+	public void verifyCountApiResponse() throws IOException {
+
+		given().baseUri(getProperty("BASE_URI")).contentType(ContentType.JSON).and().accept(ContentType.JSON)
+				.header("Authorization", getToken(Role.FD)).log().uri().and().log().method().and().log().headers()
+				.when().get("/dashboard/count").then().log().ifValidationFails().statusCode(200).and()
+				.time(lessThan(1000l)).and().body("message", Matchers.equalTo("Success")).body("data", notNullValue())
+				.body("data.size()", equalTo(3)).and().body("data.count", Matchers.everyItem(greaterThanOrEqualTo(0)))
+				.and().body("data.label", Matchers.everyItem(Matchers.not(Matchers.blankOrNullString()))).and().
+				body("data.key", Matchers.containsInAnyOrder("pending_for_delivery","created_today","pending_fst_assignment"))
+				.body(JsonSchemaValidator
+						.matchesJsonSchemaInClasspath("response-schema/countApiResponseSchema-FD.json"));
+	}
+
+	@Test
+	public void countApiTest_MissingAuthToken() throws IOException {
+		given().baseUri(getProperty("BASE_URI")).contentType(ContentType.JSON).and().accept(ContentType.JSON)
+		.log().uri().and().log().method().and().log().headers().
+		when().get("/dashboard/count").then().log().ifValidationFails().statusCode(401);
+
+	}
+
+}
---
+++ src/test/java/com/api/test/MasterApiTest.java
@@ -0,0 +1,54 @@
+package com.api.test;
+
+import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import com.api.constant.Role;
+import static com.api.utils.AuthTokenProvider.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+import static com.api.utils.ConfigManager2.*;
+
+import static io.restassured.RestAssured.*;
+
+import java.io.IOException;
+
+public class MasterApiTest {
+	
+	@Test
+	public void masterApiTest() throws IOException {
+		
+		given().
+		baseUri(getProperty("BASE_URI")).
+		contentType("").
+		and().
+		header("Authorization",getToken(Role.FD)).
+		when().post("/master").
+		then().log().all().
+		statusCode(200).time(lessThan(1000l)).and().body("message", equalTo("Success")).
+		body("data",notNullValue()).body("data",hasKey("mst_oem"))
+		.body("data",hasKey("mst_model"))
+		.body("data",hasKey("mst_oem")).body("$",hasKey("message")).body("data.mst_oem",equalTo(2)).
+		body("data.mst_oem.id",Matchers.everyItem(notNullValue())).
+		body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/masterApiResponseSchema.json"));
+		//In Rest Assured, this is a Groovy GPath expression
+		
+	}
+	
+	@Test
+	public void invalidTokenMasterApi() throws IOException {
+		given().
+		baseUri(getProperty("BASE_URI")).
+		contentType("").
+		and().
+		header("Authorization",getToken(Role.FD)).
+		when().post("/master").
+		then().log().all().
+		statusCode(401);
+	}
+
+}
