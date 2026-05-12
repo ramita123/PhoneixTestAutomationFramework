1524baa97258ca4a35b0f975086030a204979f44
ramita123 <ramitasambyal@gmail.com>
2026-05-12 16:02:03 +0530

    Updated java version from 14 to 16

--- src/test/java/com/api/test/CreateJobApiTest.java
+++ src/test/java/com/api/test/CreateJobApiTest.java
@@ -3,6 +3,8 @@ package com.api.test;
 import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
@@ -18,6 +20,7 @@ import com.api.utils.ConfigManager2;
 import com.api.utils.SpecUtil;
 
 import io.restassured.http.ContentType;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 import static io.restassured.RestAssured.*;
 
@@ -28,15 +31,20 @@ public class CreateJobApiTest {
 		
 		Customer  customer= new Customer("ramita", "sambyal", "8976546789", "", "ramitasambyal@gmail.com", "");
 		CustomerAdress customerAddress =new CustomerAdress("duplex", "duplex", "harsar", "duplex", "duplex", "176023", "india", "HP");
-		CustomerProduct customerProduct= new CustomerProduct("2026-03-14T18:30:00.000Z", "ime_8360378289", "131305999703484", "131305999793951", "2026-03-14T18:30:00.000Z", 1, 2);
+		CustomerProduct customerProduct= new CustomerProduct("2026-03-14T18:30:00.000Z", "ime_8360378289", "1364465999763484", "122405999793951", "2026-03-14T18:30:00.000Z", 1, 2);
 		Problems problesm=new Problems(1, "fixing issue");
-		Problems problemsArray[]=new Problems[1];
-		problemsArray[0]=problesm;
-		
-		CreateJobPayload createJobPayload= new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemsArray);
+		List<Problems> problemList=new ArrayList<>();
+		problemList.add(problesm);
+	
+		CreateJobPayload createJobPayload= new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemList);
 	
 		
-		given().spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload)).when().post("/job/create").then().spec(SpecUtil.responseSpec_OK()).body("message", equalTo("Job created successfully. "));
+		given().spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload)).when().post("/job/create").then().spec(SpecUtil.responseSpec_OK()).
+		body("message", equalTo("Job created successfully. ")).
+		body(matchesJsonSchemaInClasspath("response-schema/createJobResponseSchema.json")).
+		body("data.mst_service_location_id",equalTo(1)).body("data.job_number", Matchers.startsWith("JOB_"))
+		.body("data",hasKey("id"));
+		
 	//	.body(Matchers.hasProperty("data.id"));
 
 		
