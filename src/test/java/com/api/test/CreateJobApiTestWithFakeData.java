3f286720ffe706f7b01e4f94dbab20e9b32572eb
ramita123 <ramitasambyal@gmail.com>
2026-06-28 17:48:05 +0530

    Introduced Deserilization

--- src/test/java/com/api/test/CreateJobApiTestWithFakeData.java
+++ src/test/java/com/api/test/CreateJobApiTestWithFakeData.java
@@ -31,6 +31,7 @@ import com.api.request.model.Customer;
 import com.api.request.model.CustomerAdress;
 import com.api.request.model.CustomerProduct;
 import com.api.request.model.Problems;
+import com.api.response.model.CreateJobResponseModel;
 import com.api.utils.DateTimeUtility;
 import com.database.dao.CustomerAddressDao;
 import com.database.dao.CustomerDao;
@@ -64,15 +65,15 @@ public class CreateJobApiTestWithFakeData {
 	@Test(description="verifying if create job api is able to create inwarranty job",groups= {"api","regression","smoke"})
 	public void createJobApiTest() throws IOException {
 		
-	Response response=given().spec(requestSpecWithAuth(Role.FD, createJobPayload)).when().post("/job/create").then().spec(responseSpec_OK()).
+		CreateJobResponseModel createJobResponseModel=given().spec(requestSpecWithAuth(Role.FD, createJobPayload)).when().post("/job/create").then().spec(responseSpec_OK()).
 		body("message", equalTo("Job created successfully. ")).
 		body(matchesJsonSchemaInClasspath("response-schema/createJobResponseSchema.json")).
 		body("data.mst_service_location_id",equalTo(1)).body("data.job_number", startsWith("JOB_"))
 		.body("data",hasKey("id"))
-		.extract().response();
+		.extract().response().as(CreateJobResponseModel.class);
 	
-	int customerId=response.jsonPath().getInt("data.tr_customer_id");
-	int customerProductId=response.jsonPath().getInt("data.tr_customer_product_id");
+	int customerId=createJobResponseModel.getData().getTr_customer_id();
+	int customerProductId=createJobResponseModel.getData().getTr_customer_product_id();
 	
 	
 	System.out.println("**************************"+customerId);
