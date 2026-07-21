f4c8c27d030632ee22384e192b1da914cf15dc0a
ramita123 <ramitasambyal@gmail.com>
2026-07-21 08:30:56 +0530

    Added allure annotations to the user details test and login api test

--- src/test/java/com/api/test/UserDetailsApiTest.java
+++ src/test/java/com/api/test/UserDetailsApiTest.java
@@ -19,11 +19,19 @@ import static com.api.utils.AuthTokenProvider.*;
 
 import static  com.api.utils.ConfigManager2.*;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 
-
+@Epic("User Management")
+@Feature("USer Details")
 @Listeners(com.listeners.ApiTestListerner.class)
+
 public class UserDetailsApiTest {
 	
 	private UserService userService;
@@ -33,6 +41,10 @@ public class UserDetailsApiTest {
 		 userService= new UserService();
 	}
 	
+	
+	@Story("User details should be shown")
+	@Description("verify user details response shown correctly and it belongs to the group ")
+	@Severity(SeverityLevel.CRITICAL)
 	
 	@Test(description="verify user details response shown correctly and it belongs to the group ",groups= {"api","regression","smoke"})
 	public void userDetailsAPITest() throws IOException {
